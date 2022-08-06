507f8de6bc38ee582a9300a4a0bf878567f7014c
Vinicius Santos <vnniciussantos@gmail.com>
2022-08-06 20:48:20 -0300

    Criando componente com filhos

--- src/index.js
+++ src/index.js
@@ -2,15 +2,24 @@ import './index.css'
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-import Primeiro from './components/Primeiro.jsx'
-import ComParametro from './components/ComParametro'
+// import Primeiro from './components/Primeiro.jsx'
+// import ComParametro from './components/ComParametro'
+import ComFilhos from './components/ComFilhos'
 
 ReactDOM.render(
     <div>
-        <Primeiro/>
+        <ComFilhos>
+            <ul>
+                <li>Ana</li>
+                <li>Bia</li>
+                <li>Carlos</li>
+                <li>Daniel</li>
+            </ul>
+        </ComFilhos>
+        {/* <Primeiro/>
         <ComParametro titulo="Esse é o titulo"
             subtitulo="Esse é o subtitulo"/>
-        <ComParametro titulo="Opa" subtitulo="Epa"/>
+        <ComParametro titulo="Opa" subtitulo="Epa"/> */}
     </div>,
     document.getElementById('root')
 )

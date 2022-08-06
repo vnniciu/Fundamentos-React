22f59df98e6ef2a421d0dd3d46c53c128fb4f082
Vinicius Santos <vnniciussantos@gmail.com>
2022-08-06 20:19:52 -0300

    Refatorando componente de maneira mais enxuta com Arrow

--- src/components/Primeiro.jsx
+++ src/components/Primeiro.jsx
@@ -1,12 +1,7 @@
 import React from "react";
 
-function Primeiro() {
-    return (
-        <div>
-            <h1>Primeiro Componente</h1>
+export default () =>
+        <>
+            <h1>Primeiro Componente (Arrow)</h1>
             <h2>Exemplo de um componente React</h2>
-        </div>
-    )
-}
-
-export default Primeiro;
+        </>

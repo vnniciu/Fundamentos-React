19835c95a819e8fb8a5073d48507ae498c0e138a
Vinicius Santos <vnniciussantos@gmail.com>
2022-08-07 00:52:03 -0300

    Criado componente App

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import './App.css'
+import React from "react";
+
+import Primeiro from './components/Primeiro.jsx'
+import ComParametro from './components/ComParametro'
+import ComFilhos from './components/ComFilhos'
+import Card from './components/layout/Card'
+
+export default (props) => (
+    <div className='App'>
+        <Card titulo="#03 - Componente com Filhos">
+            <ComFilhos>
+                <ul>
+                    <li>Ana</li>
+                    <li>Bia</li>
+                    <li>Carlos</li>
+                    <li>Daniel</li>
+                </ul>
+            </ComFilhos>
+        </Card>
+        <Card titulo="#02 - Componente com Parametro">
+            <ComParametro titulo="Esse é o titulo"
+                subtitulo="Esse é o subtitulo"/>
+        </Card>
+        <Card titulo="#01 - Primeiro Componente">
+            <Primeiro/>
+        </Card>
+    </div>
+);
--- src/index.js
+++ src/index.js
@@ -2,31 +2,9 @@ import './index.css'
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-import Primeiro from './components/Primeiro.jsx'
-// import ComParametro from './components/ComParametro'
-// import ComFilhos from './components/ComFilhos'
-import Card from './components/layout/Card'
+import App from './App'
 
 ReactDOM.render(
-    <div>
-        <Card titulo="Primeiro Componente">
-            <Primeiro/>
-        </Card>
-        <Card titulo="Exercicio X">
-            Conteudo
-        </Card>
- {/*        <ComFilhos>
-            <ul>
-                <li>Ana</li>
-                <li>Bia</li>
-                <li>Carlos</li>
-                <li>Daniel</li>
-            </ul>
-        </ComFilhos>
-  */}       {/* <Primeiro/>
-        <ComParametro titulo="Esse é o titulo"
-            subtitulo="Esse é o subtitulo"/>
-        <ComParametro titulo="Opa" subtitulo="Epa"/> */}
-    </div>,
+    <App></App>,
     document.getElementById('root')
 )

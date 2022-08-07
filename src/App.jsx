d15de012c226e22ac1187a8d9bfd3fe092c1255d
Vinicius Santos <vnniciussantos@gmail.com>
2022-08-07 01:26:46 -0300

    Criado componente Repeticao

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,13 @@ import Primeiro from './components/Primeiro.jsx'
 import ComParametro from './components/ComParametro'
 import ComFilhos from './components/ComFilhos'
 import Card from './components/layout/Card'
+import Repeticao from './components/Repeticao';
 
 export default (props) => (
     <div className='App'>
+        <Card titulo="#04 - Repetição">
+            <Repeticao></Repeticao>
+        </Card>
         <Card titulo="#03 - Componente com Filhos">
             <ComFilhos>
                 <ul>

d6a008cff8dc0dc0beba2f445bedf0faccbf21d3
Vinicius Santos <vnniciussantos@gmail.com>
2022-08-07 01:41:28 -0300

    Criando condicionais de duas formas

--- src/App.jsx
+++ src/App.jsx
@@ -6,9 +6,17 @@ import ComParametro from './components/ComParametro'
 import ComFilhos from './components/ComFilhos'
 import Card from './components/layout/Card'
 import Repeticao from './components/Repeticao';
+import Condicional from './components/Condicional';
+import CondicionalComIf from './components/CondicionalComIf';
 
 export default (props) => (
     <div className='App'>
+        <Card titulo="#06 - Condicional v2">
+            <CondicionalComIf numero={11}> </CondicionalComIf>
+        </Card>
+        <Card titulo="#05 - Condicional v1">
+            <Condicional numero={10}> </Condicional>
+        </Card>
         <Card titulo="#04 - Repetição">
             <Repeticao></Repeticao>
         </Card>
